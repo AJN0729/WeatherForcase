0ecc765c2866837eb12d6e8e645474a21432e7d0
AJN0729 <andrewn0729@student.k12.tulsaschools.org>
2021-11-10 10:02:56 -0600

    API file is done.

---
+++ WeatherForcase/Networking/API.swift
@@ -0,0 +1,12 @@
+//
+//  API.swift
+//  WeatherForcase
+//
+//  Created by AMStudent on 11/10/21.
+//
+
+import Foundation
+
+struct API {
+    static let key = "d7801307f0dfac03c17991b8259c14e2"
+}
