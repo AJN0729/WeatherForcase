8f8d8b9dd776ee0242e786635added0e9a3dc968
AJN0729 <andrewn0729@student.k12.tulsaschools.org>
2021-11-10 10:31:51 -0600

    Networking is done.

--- WeatherForcase/Networking/API+Extension.swift
+++ WeatherForcase/Networking/API+Extension.swift
@@ -11,6 +11,6 @@ extension API {
     static let baseURLString = "https://api.openweathermap.org/data/2.5/"
     
     static func getURLfor(lat: Double, lon: Double) -> String {
-        return ("\(baseURLString)onecall?lat=\(lat)&lon=\(lon)&exclude=minutely&appid=\(key)&units=imperial")
+        return "\(baseURLString)onecall?lat=\(lat)&lon=\(lon)&exclude=minutely&appid=\(key)&units=imperial"
     }
 }
