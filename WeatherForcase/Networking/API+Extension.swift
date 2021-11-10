5b3fe585e4efcfb5508b984ae7f04582fe8e5a4d
AJN0729 <andrewn0729@student.k12.tulsaschools.org>
2021-11-10 10:14:42 -0600

    API+Extension are done.

---
+++ WeatherForcase/Networking/API+Extension.swift
@@ -0,0 +1,16 @@
+//
+//  API+Extension.swift
+//  WeatherForcase
+//
+//  Created by AMStudent on 11/10/21.
+//
+
+import Foundation
+
+extension API {
+    static let baseURLString = "https://api.openweathermap.org/data/2.5/"
+    
+    static func getURLfor(lat: Double, lon: Double) -> String {
+        return ("\(baseURLString)onecall?lat=\(lat)&lon=\(lon)&exclude=minutely&appid=\(key)&units=imperial")
+    }
+}
