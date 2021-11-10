e4a0eb9eaaddf12dba24e1d339dd0bc59fac682a
AJN0729 <andrewn0729@student.k12.tulsaschools.org>
2021-11-10 09:24:37 -0600

    Initial Commit

---
+++ WeatherForcase/WeatherForcaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherForcaseApp.swift
+//  WeatherForcase
+//
+//  Created by AMStudent on 11/10/21.
+//
+
+import SwiftUI
+
+@main
+struct WeatherForcaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
