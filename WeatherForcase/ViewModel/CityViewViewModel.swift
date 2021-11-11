fee2568513bc922f1ca06ef9691dfcf960034467
AJN0729 <andrewn0729@student.k12.tulsaschools.org>
2021-11-11 10:43:59 -0600

    finished CityViewViewModel file.

--- WeatherForcase/ViewModel/CityViewViewModel.swift
+++ WeatherForcase/ViewModel/CityViewViewModel.swift
@@ -14,7 +14,7 @@ final class CityViewViewModel: ObservableObject {
     
     @Published var city: String = "Tulsa" {
         didSet {
-//            getLocation()
+            getLocation()
         }
     }
     
@@ -37,7 +37,7 @@ final class CityViewViewModel: ObservableObject {
     }()
     
     init() {
-//        getLocation()
+        getLocation()
     }
     
     var date: String {
@@ -56,7 +56,7 @@ final class CityViewViewModel: ObservableObject {
     }
     
     var conditions: String {
-        if weather.current.weather > 0 {
+        if weather.current.weather.count > 0 {
             return weather.current.weather[0].main
         }
         return ""
@@ -73,4 +73,131 @@ final class CityViewViewModel: ObservableObject {
     var rainChances: String {
         return String(format: "%0.0%%", weather.current.dew_point)
     }
+    
+    func getTimeFor(timestamp: Int) -> String {
+        return timeFormatter.string(from: Date(timeIntervalSince1970: TimeInterval(timestamp)))
+    }
+    
+    func getTempFor(temp: Double) -> String {
+        return String(format: "%0.1f", temp)
+    }
+    
+    func getDayFor(timestamp: Int) -> String {
+        return dayFormatter.string(from: Date(timeIntervalSince1970: TimeInterval(timestamp)))
+    }
+    
+    private func getLocation() {
+        CLGeocoder().geocodeAddressString(city) { (placemarks, error) in
+            if let places = placemarks, let place = places.first {
+            self.getWeather(coord: place.location?.coordinate)
+        }
+    }
 }
+    
+    private func getWeather(coord: CLLocationCoordinate2D?) {
+        if let coord = coord {
+            let urlString = API.getURLfor(lat: coord.latitude, lon: coord.longitude)
+            getWeatherInternal(city: city, for: urlString)
+        } else {
+            let urlString = API.getURLfor(lat: 36.153980, lon: -95.992775)
+            getWeatherInternal(city: city, for: urlString)
+        }
+    }
+    
+    private func getWeatherInternal(city: String, for urlString: String) {
+        NetworkManager<WeatherResponse>.fetch(for: URL(string: urlString)!) { (result) in
+            switch result {
+        case . success(let response):
+            DispatchQueue.main.async {
+                self.weather = response
+            }
+        case .failure(let err):
+            print(err)
+            }
+        }
+    }
+    
+    func getLottieAnimation(icon: String) -> String {
+         switch icon {
+         case "01d":
+             return "dayClearSky"
+         case "01n" :
+             return "nightClearSky"
+         case "02n" :
+             return "nightFewClouds"
+         case "03d" :
+             return "dayScatteredClouds"
+         case "04d" :
+             return "nightScatteredClouds"
+         case "04n" :
+             return "nightBrokenClouds"
+         case "09d" :
+             return "dayShowerRains"
+         case "09n" :
+             return "nightShowerRains"
+         case "10d" :
+             return "dayRain"
+         case "10n" :
+             return "nightRain"
+         case "11d" :
+             return "dayThunderstorm"
+         case "11n" :
+             return "nightThunderstorm"
+         case "13d" :
+             return "daySnow"
+         case "13n" :
+             return "nightSnow"
+         case "50d" :
+             return "dayMist"
+         case "50n" :
+             return "nightMist"
+         default:
+             return "dayClearSky"
+         }
+     }
+     
+     func getWeatherIconFor(icon: String) -> Image {
+         switch icon {
+         case "01d" :
+             return Image(systemName: "sun.max.fill")
+         case "01n" :
+             return Image(systemName: "moon.fill")
+         case "02d" :
+             return Image(systemName: "cloud.sun.fill")
+         case "02n" :
+             return Image(systemName: "cloud.moon.fill")
+         case "03d" :
+             return Image(systemName: "cloud.fill")
+         case "03n" :
+             return Image(systemName: "cloud.fill")
+         case "04d" :
+             return Image(systemName: "cloud.fill")
+         case "04n" :
+             return Image(systemName: "cloud.fill")
+         case "09d" :
+             return Image(systemName: "cloud.drizzle.fill")
+         case "09n" :
+             return Image(systemName: "cloud.drizzle.fill")
+         case "10d" :
+             return Image(systemName: "cloud.heavyrain.fill")
+         case "10n" :
+             return Image(systemName: "cloud.heavyrain.fill")
+         case "11d" :
+             return Image(systemName: "cloud.bolt.fill")
+         case "11n" :
+             return Image(systemName: "cloud.bolt.fill")
+         case "13d" :
+             return Image(systemName: "cloud.snow.fill")
+         case "13n" :
+             return Image(systemName: "cloud.snow.fill")
+         case "50d" :
+             return Image(systemName: "cloud.fog.fill")
+         case "50n" :
+             return Image(systemName: "cloud.fog.full")
+         default:
+             return Image (systemName: "sun.max.fill")
+             
+         }
+     }
+}
+
